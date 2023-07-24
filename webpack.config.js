30d0551cecff92549e94a89ec87662772186630f
Ratnesh Yadav <106252196+Ratnesh0@users.noreply.github.com>
2023-07-24 11:51:30 +0530

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/index.html', // Make sure the template points to index.html
     }),
   ],
   output: {
